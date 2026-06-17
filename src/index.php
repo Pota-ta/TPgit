077ee53eb574ffd51caf27e75e7d588424542b75
Pota-ta <loveandriatsilavo@gmail.com>
2026-06-17 13:27:16 +0300

    index

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php
+// Page d'accueil simple
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Base Employees</title>
+</head>
+<body>
+    <h1>TP Employees</h1>
+    <ul>
+        <li><a href="departments.php">Liste des départements</a></li>
+        <li><a href="employees.php?dept_no=d001">Exemple : employés du département d001</a></li>
+        <li><a href="employee.php?emp_no=10001">Exemple : fiche employé 10001</a></li>
+    </ul>
+</body>
+</html>
