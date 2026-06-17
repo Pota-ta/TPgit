b9018133ba6286d9812cc950fe6ab77fe21326a8
Pota-ta <loveandriatsilavo@gmail.com>
2026-06-17 13:33:12 +0300

    correction db nom

--- src/utils/connexion.php
+++ src/utils/connexion.php
@@ -3,7 +3,7 @@
 define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'test');
+define('DB_NAME', 'employees');
 define('DB_CHARSET', 'utf8');
 
 function getConnexion()
