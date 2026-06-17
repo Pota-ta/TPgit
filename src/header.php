d23cf67b2bf50ccb3f681b9dd55ac1c0db530796
Pota-ta <loveandriatsilavo@gmail.com>
2026-06-17 13:26:45 +0300

    head et foot

---
+++ src/header.php
@@ -0,0 +1,12 @@
+<?php
+if (!isset($page_title)) {
+    $page_title = 'Employees';
+}
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title><?= htmlspecialchars($page_title) ?></title>
+</head>
+<body>
